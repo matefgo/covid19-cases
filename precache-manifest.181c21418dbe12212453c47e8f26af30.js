7acba2c884e8eff2e1fdadee3b06e7887e4fd47d
matheus.granville <matheusgranville@gmail.com>
2020-09-08 00:23:06 -0300

    Updates

---
+++ precache-manifest.181c21418dbe12212453c47e8f26af30.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a46afa5c851296a7e1c0428cc4acf2c4",
+    "url": "/covid19-cases/index.html"
+  },
+  {
+    "revision": "99780632fdadbf10e975",
+    "url": "/covid19-cases/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1ca4bdedfe0296b8aab8",
+    "url": "/covid19-cases/static/js/2.89894d0e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid19-cases/static/js/2.89894d0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99780632fdadbf10e975",
+    "url": "/covid19-cases/static/js/main.d6f0968f.chunk.js"
+  },
+  {
+    "revision": "de03fc265c737b4a715e",
+    "url": "/covid19-cases/static/js/runtime-main.f235ad11.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid19-cases/static/media/logo.5d5d9eef.svg"
+  }
+]);
